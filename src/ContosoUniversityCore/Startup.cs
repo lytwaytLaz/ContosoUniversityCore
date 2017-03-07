23e133a93a4e19e1026519de61d6fdcbfa0a2f1a
lytwaytLaz <laszlo.hago@gmail.com>
2017-03-07 10:51:57 +0100

    Add code first db creation and controller for Student

--- src/ContosoUniversityCore/Startup.cs
+++ src/ContosoUniversityCore/Startup.cs
@@ -45,6 +45,9 @@ namespace ContosoUniversityCore
             // Add framework services.
             services.AddApplicationInsightsTelemetry(Configuration);
 
+            services.AddDbContext<SchoolContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
@@ -60,7 +63,7 @@ namespace ContosoUniversityCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, SchoolContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
@@ -92,6 +95,8 @@ namespace ContosoUniversityCore
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+
+            DbInitializer.Initialize(context);
         }
     }
 }
