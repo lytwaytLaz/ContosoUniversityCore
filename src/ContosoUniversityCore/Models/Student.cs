c23e518965af5b3a7ee93ef0ab232ee65289c5a9
lytwaytLaz <laszlo.hago@gmail.com>
2017-03-15 20:26:07 +0100

    Add NativeLanguage to DB table Students, and dropdown list for choosing language from enum Languages in Students class

--- src/ContosoUniversityCore/Models/Student.cs
+++ src/ContosoUniversityCore/Models/Student.cs
@@ -4,13 +4,24 @@ using System.Collections.Generic;
 
 namespace ContosoUniversityCore.Models
 {
+    public enum Languages
+    {
+        Arabic,
+        English,
+        French,
+        Hungarian,
+        Italian,
+        Japanese,
+        Swedish,
+    }
     public class Student
     {
         public int ID { get; set; }
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
-        
+        public Languages NativeLanguage { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
